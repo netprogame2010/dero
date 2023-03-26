09778b71beb9d989f2ba7bff30bd4c0cbc7ce636
TaTaiGame <110656863+netprogame2010@users.noreply.github.com>
2023-03-26 20:13:58 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Replace deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe3lghx7tmyel9s3uegy2, community-pools.mysrv.cloud:10300 to run the miner"
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
     ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe3lghx7tmyel9s3uegy2 -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
